00c1de6bdb592376b83c6ea82800f209a1d85dfc
unknown <youngerous@gmail.com>
2020-07-06 15:55:04 +0900

    add test module

--- src-pytorch/lightning_engineering.py
+++ src-pytorch/lightning_engineering.py
@@ -14,30 +14,49 @@ class PrintCallback(pl.Callback):
         print('Training is done.')
 
 def main(hparams):
-    model = Baseline(hparams)
+    model = Baseline(vars(hparams))
     print_callback = [PrintCallback()]
     trainer = pl.Trainer(
                 gpus=hparams.gpus,
                 callbacks=print_callback,
                 max_epochs=hparams.epoch,
-                logger=TensorBoardLogger(save_dir='./logs', name='musinsa'),
+                logger=TensorBoardLogger(save_dir='./logs', name='train'),
+                early_stop_callback=True,
                 distributed_backend=hparams.distributed_backend
             )
-    trainer.fit(model)
+
+    if hparams.mode.lower() == 'train': # train + test
+        trainer.fit(model)  
+    elif hparams.mode.lower() == 'test': # only test
+        model = model.load_from_checkpoint(
+                    checkpoint_path='logs\musinsa\\version_35\checkpoints\epoch=6.ckpt'
+                )
+    else:
+        raise ValueError('You must choose train or test mode')
+
+    ## pytorch-lightning BUG: cannot apply distributed-backend option
+    trainer = pl.Trainer(
+            gpus=hparams.gpus,
+            callbacks=print_callback,
+            max_epochs=hparams.epoch,
+            logger=TensorBoardLogger(save_dir='./logs', name='test'),
+            early_stop_callback=True,
+        )
+    trainer.test(model)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpus', type=int, default=1, help='number of gpus')
-    parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')
+    parser.add_argument('--lr', type=float, default=0.00001, help='learning rate')
     parser.add_argument('--batch_size', type=int, default=64, help='batch size')
     parser.add_argument('--epoch', type=int, default=200, help='epochs to train')
     parser.add_argument('--seed', type=int, default=711, help='random seed')
     parser.add_argument('--path', type=str, default='D:/data/musinsa/train_test_valid', help='parent directory containing train, val, test data')
     parser.add_argument('--num_classes', type=int, default=2, help='output class number')
     parser.add_argument('--distributed_backend', type=str, default='dp')
-    parser.add_argument('--model', type=str, default='resnet')
+    parser.add_argument('--model', type=str, default='mobilenet')
+    parser.add_argument('--mode', type=str, default='train', help='train or test')
 
     args = parser.parse_args()
     main(args)
 
-## TODO: how to save hparams.yaml automatically
